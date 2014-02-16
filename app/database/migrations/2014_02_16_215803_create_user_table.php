082f151dbc360a15d0e99bf7da600c2a3181c73b
Ryan Sandler <Sandler@Ryans-MacBook-Pro-2.local>
2014-02-16 17:44:03 -0500

    ryan changes ignore

--- app/database/migrations/2014_02_16_215803_create_user_table.php
+++ app/database/migrations/2014_02_16_215803_create_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -30,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('courses');
+		Schema::drop('users');
 	}
 
 }
