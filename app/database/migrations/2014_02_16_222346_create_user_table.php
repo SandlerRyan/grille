677e41085b75d2d138b357f29b40a282b3cd9b7b
nyassin <nyassin14@gmail.com>
2014-02-16 17:29:23 -0500

    synced with upates

--- app/database/migrations/2014_02_16_222346_create_user_table.php
+++ app/database/migrations/2014_02_16_222346_create_user_table.php
@@ -13,8 +13,16 @@ class CreateUserTable extends Migration {
 	public function up()
 	{
 		//
+		Schema::create('users', function($table)
+ 		{
+ 			$table->increments('id');
+ 			$table->string('name');
+ 			$table->smallInteger('phone_number');
+ 			$table->boolean('hours_notification');
+ 			$table->boolean('deals_notification');
+             $table->timestamps();
+		}
 	}
-
 	/**
 	 * Reverse the migrations.
 	 *
@@ -22,7 +30,8 @@ class CreateUserTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('users');
+
 	}
 
 }
