676e95bc5fc2d04958d5fb9d26e57040223d1ba0
Vladimir Bok <vladimir@kensho.com>
2014-02-17 14:11:22 -0500

    added sample AngularJS application. modified from: http://www.codeproject.com/Articles/576246/A-Shopping-Cart-Application-Built-with-AngularJS

---
+++ AngularGrille/js/product.js
@@ -0,0 +1,9 @@
+﻿//----------------------------------------------------------------
+// product class
+function product(id, name, description, price, avaiable) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.avaiable = true;
+}
