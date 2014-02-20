ba5916d6acf040e6bbdc044219cd1129a05d7ab3
Vladimir Bok <vladimir@kensho.com>
2014-02-19 21:00:43 -0500

    removed AngularGrille

--- AngularGrille/js/product.js
+++
@@ -1,9 +0,0 @@
-﻿//----------------------------------------------------------------
-// product class
-function product(id, name, description, price, avaiable) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.avaiable = true;
-}
