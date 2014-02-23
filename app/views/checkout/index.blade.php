4459055b381beac7e2cad0a99191443b012fe8b1
Ian Boothby <iboothby@college.harvard.edu>
2014-02-23 11:22:07 -0500

    completed js for note handling

--- app/views/checkout/index.blade.php
+++ app/views/checkout/index.blade.php
@@ -1,6 +1,8 @@
-<?php
-var_dump(Cart::contents());
-?>
+<div class="result">
+	<?php
+	var_dump(Cart::contents());
+	?>
+</div>
 
 THESE ARE YOUR ORDER DETAILS!
 
@@ -24,31 +26,58 @@ Enter a Note:
 <a class="btn btn-lg btn-success" href="/order/create" role="button">Back to Menu</a>
 
 <script>
-$(".addNote").click(function() {
-	console.log("hi");
-	var id = this.id;
-	containerID = "#text-" + id;
-	var value = $(containerID).val();
-	console.log(id);
-	console.log(value);
-	$.ajax({
-		url: "/add_note/" + id + "/" + value,
-		type: "get",
-		error: function(){
-			alert("failure");
-			$("#result").html('There was an error during submission');
-		},
-		success: function(data){
-			// remove the input box and add note button;
-			// replace with the note and an "edit note"
-			$("#" + id).replaceWith(
-				'<button type="button" class="addNote" id="' + id + 
-				'">Edit Note</button>');
-			$(containerID).replaceWith(
-				'<p>' + value + '</p>');
-		}
+// makes an ajax call to the database to add a note
+function add_note ()
+{
+	$(".addNote").click(function() {
+		var id = this.id;
+		containerID = "#text-" + id;
+		var value = $(containerID).val();
+		$.ajax({
+			url: "/add_note/" + id + "/" + value,
+			type: "get",
+			error: function(){
+				console.log('error');
+				alert("failure");
+				$("#result").html('There was an error during submission');
+			},
+			success: function(data){
+				// remove the input box and add note button;
+				// replace with the note and an "edit note"
+				console.log('success');
+				$("#" + id).replaceWith(
+					'<button type="button" class="editNote" id="' + id + 
+					'">Edit Note</button>');
+				$(containerID).replaceWith(
+					'<p id="text-' + id + '">' + value + '</p>');
+				// attach the edit note handler to the new DOM element
+				edit_note();
+			}
+		});
 	});
-});
+}
 
+// changes the text back to an input box for editing
+function edit_note ()
+{
+	$(".editNote").click(function() {
+		var id = this.id;
+		containerID = "#text-" + id;
+		var value = $(containerID).text();
+		$("#" + id).replaceWith(
+			'<button type="button" class="addNote" id="' + id + 
+			'">Submit Note</buton>');
+		$(containerID).replaceWith(
+			'<input type="text" class="note" id="text-' + id + 
+			'" value="' + value + '" maxlength="250"/>');
+		// attach the add note handler to the new DOM element
+		add_note();
+	});
+}
+
+$(document).ready(function () {
+	add_note();
+	edit_note();
+});
 
 </script>
