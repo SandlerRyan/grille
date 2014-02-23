9e02d73eacb9a8b17d2dc6234359e84f20a2f654
Ian Boothby <iboothby@college.harvard.edu>
2014-02-23 15:46:11 -0500

    forgot to add

---
+++ app/database/migrations/2014_02_23_175909_create_item_order_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemOrderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('item_orders', function($table)
+		{
+			$table->integer('order_id')->unsigned();
+			$table->foreign('order_id')->references('id')->on('orders');
+
+			$table->integer('item_id')->unsigned();
+			$table->foreign('item_id')->references('id')->on('items');
+
+			$table->integer('addon_id')->unsigned();
+			$table->foreign('addon_id')->references('id')->on('addons');
+			
+			$table->integer('quantity');
+			$table->string('notes');
+         });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('item_orders');
+	}
+
+}
